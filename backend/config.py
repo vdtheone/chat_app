8b3969c0701b762087fd630fcf304aa358b1095e
vdtheone <vishalprajapati22052001@gmail.com>
2023-12-05 18:38:54 +0530

    create configuration for database connectivity

---
+++ backend/config.py
@@ -0,0 +1,15 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SQLALCHEMY_DATABASE_URL  = os.environ.get('DATABASR_URL')
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
