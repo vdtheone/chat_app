32f788157b9ceef7cb5eedf8c834ab4f7c02d97f
vdtheone <vishalprajapati22052001@gmail.com>
2023-12-07 12:49:37 +0530

    refactor code and seprate logic part.

--- backend/chat/routers/user.py
+++ backend/chat/routers/user.py
@@ -1,5 +1,12 @@
 import json
-from fastapi import APIRouter, Depends, HTTPException, Request, WebSocket, WebSocketDisconnect
+from fastapi import (
+    APIRouter,
+    Depends,
+    HTTPException,
+    Request,
+    WebSocket,
+    WebSocketDisconnect,
+)
 from fastapi.responses import HTMLResponse
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
@@ -10,7 +17,17 @@ from chat.models.user_info import User
 from chat.schemas.user import UserCreateSchema
 from config import SessionLocal
 from typing import List
-from chat.services.user import Oauth2Login, create_new_user
+from chat.services.user import (
+    Oauth2Login,
+    create_new_user,
+    handle_cleanup,
+    handle_disconnect,
+    handle_group_chat_cleanup,
+    handle_group_chat_connection,
+    handle_group_chat_message,
+    handle_received_message,
+    handle_websocket_connection,
+)
 
 user_router = APIRouter()
 
@@ -23,141 +40,64 @@ def get_db():
         db.close()
 
 
-active_connections = {}
-web_list = []
-user_statuses = {}
-
-
-async def authenticate_user(websocket: WebSocket, db: Session):
-    
-    jwt_token = websocket.headers.get("Authorization")
-    user_id = decode_jwt_token(jwt_token)
-    if user_id:
-        return user_id
-    else:
-        raise HTTPException(status_code=401, detail="Invalid or missing token")
-
-
-async def handle_websocket_communication(
-    websocket: WebSocket, user_id: int, id: int, db: Session
-):
-    try:
-        while True:
-            pass
-    except Exception as e:
-        if user_id in active_connections:
-            # user_statuses[user_id] = "offline"
-            # await send_user_status_update(user_id, "offline")
-            del active_connections[user_id]
-    finally:
-        if user_id in active_connections:
-            # user_statuses[user_id] = "offline"
-            # await send_user_status_update(user_id, "offline")
-            del active_connections[user_id]
-            await websocket.close()
-
-
-
-
-async def send_user_status_update(user_id: int, status: str):
-    websocket = active_connections.get(str(user_id))
-    if websocket:
-        # Construct and send status update message
-        status_message = {"user_id": user_id, "status": status}
-        await websocket.send_json(status_message)
-
-
-# WebSocket disconnection event handler
-async def websocket_disconnect(user_id: int):
-    if user_id in active_connections:
-        del active_connections[user_id]
-        user_statuses[user_id] = "offline"
-        await send_user_status_update(user_id, "offline")
-            
-
-
 @user_router.websocket("/ws/{id}")
 async def websocket_endpoint_personal_chat(
     websocket: WebSocket, id: int, db: Session = Depends(get_db)
 ):
     await websocket.accept()
+    user_id = None
+
     try:
-        web_list.append(websocket)
-        jwt_token = websocket.headers.get("Authorization")
-        user_id = decode_jwt_token(jwt_token)
-        active_connections[str(user_id)] = websocket
-        # user_statuses[user_id] = "online"
-        status_message = {"user_id": user_id, "status": "online"}
-        await websocket.send_json(status_message)  
-        data = await websocket.receive_text()
-        # Handle the message
-        if str(id) in active_connections:
-            new_message = Message(sender_id=user_id, receiver_id=id, content=data)
-            db.add(new_message)
-            db.commit()
-            db.refresh(new_message)
-            await broadcast_message(data, active_connections[str(id)])
-    # except HTTPException as e:
-    #     if user_id in active_connections:
-    #         del active_connections[user_id]
-    #     print("HTTP")
-    #         # user_statuses[user_id] = "offline"
-    #         # await send_user_status_update(user_id, "offline")
+        user_id = await handle_websocket_connection(websocket, id)
+
+        while True:
+            data = await websocket.receive_text()
+            await handle_received_message(data, user_id, id, db)
+
+    except HTTPException as e:
+        print("HTTP Exception: ", str(e))
+
     except WebSocketDisconnect:
-        print("===========================================")
-        if str(user_id) in active_connections:  
-            status_message = {"user_id": user_id, "status": "offline"}
-            await websocket.send_json(status_message)
-        
+        await handle_disconnect(user_id)
+
     except Exception as e:
-        status_message = {"user_id": user_id, "status": "offline"}
-        await websocket.send_json(status_message)
-        if str(user_id) in active_connections:   
-            del active_connections[user_id]
-        print("EXCEPTIPON")
+        print("Exception: ", str(e))
+
     finally:
-        # breakpoint()
-        if websocket.client_state == "connected":
-            del active_connections[user_id]
-            await websocket.close()
-        # user_statuses[user_id] = "offline"
+        await handle_cleanup(websocket, user_id)
         print("Finally")
-
-        # if user_id in active_connections:
-        #     user_statuses[user_id] = "offline"
-        #     await send_user_status_update(user_id, "offline")
-
-
-
-# Check user online status
-def is_user_online(user_id: int) -> bool:
-    return user_id in active_connections
-
 
 
 @user_router.websocket("/ws/chat/group")
 async def websocket_group_chat(websocket: WebSocket):
     try:
-        # breakpoint()
-        await websocket.accept()
-        web_list.append(websocket)
-        active_connections['group'] = websocket
+        await handle_group_chat_connection(websocket)
 
         while True:
-            # Receive message from a client in the group chat
-            data = await websocket.receive_text()
-            for active_connection in active_connections:
-                await broadcast_message(data, active_connections[active_connection])
-            # Broadcast the received message to all connected clients (group chat)
+            await handle_group_chat_message(websocket)
+
     except Exception as e:
         print(f"Error handling WebSocket communication: {str(e)}")
     finally:
-        if 'group' in active_connections:
-            del active_connections['group']
+        await handle_group_chat_cleanup()
 
 
-async def broadcast_message(message, sender_websocket: WebSocket):
-    await sender_websocket.send_text(message)
+# @user_router.websocket("/ws/chat/group")
+# async def websocket_group_chat(websocket: WebSocket):
+#     try:
+#         await websocket.accept()
+#         web_list.append(websocket)
+#         active_connections["group"] = websocket
+
+#         while True:
+#             data = await websocket.receive_text()
+#             for active_connection in active_connections:
+#                 await broadcast_message(data, active_connections[active_connection])
+#     except Exception as e:
+#         print(f"Error handling WebSocket communication: {str(e)}")
+#     finally:
+#         if "group" in active_connections:
+#             del active_connections["group"]
 
 
 @user_router.post("/login")
@@ -176,62 +116,3 @@ async def read_users_me(current_user: User = Depends(get_current_user)):
 @user_router.post("/create_user")
 def new_user(request: Request, user: UserCreateSchema, db: Session = Depends(get_db)):
     return create_new_user(request, user, db)
-
-
-# @user_router.websocket("/ws")
-# async def websocket_endpoint(websocket: WebSocket):
-#     await websocket.accept()
-#     active_connections.append(websocket)
-
-#     try:
-#         while True:
-#             data = await websocket.receive_text()
-#             message = f"User {id(websocket)}: {data}"  # Include user identification in the message if needed
-
-#             # Broadcast the received message to all connected clients
-#             for connection in active_connections:
-#                 await connection.send_text(message)
-#     finally:
-#         # Remove the connection from the list when the client disconnects
-#         active_connections.remove(websocket)
-
-
-# # HTML for chat interface
-# html_content = """
-# <!DOCTYPE html>
-# <html>
-# <head>
-#     <title>FastAPI Chat</title>
-# </head>
-# <body>
-#     <h1>FastAPI Chat</h1>
-#     <div id="messages"></div>
-#     <input type="text" id="message" placeholder="Type a message...">
-#     <button onclick="sendMessage()">Send</button>
-
-#     <script>
-#         var ws = new WebSocket("ws://localhost:8000/chat/ws");
-
-#         ws.onmessage = function(event) {
-#             var messagesDiv = document.getElementById('messages');
-#             messagesDiv.innerHTML += '<p><strong>Friend:</strong> ' + event.data + '</p>';
-#         };
-
-#         function sendMessage() {
-#             var messageInput = document.getElementById('message');
-#             var message = messageInput.value.trim();
-#             if (message !== '') {
-#                 ws.send(message);
-#                 var messagesDiv = document.getElementById('messages');
-#                 messagesDiv.innerHTML += '<p><strong>You:</strong> ' + message + '</p>';
-#                 messageInput.value = '';
-#             }
-#         }
-#     </script>
-# </body>
-# </html>
-# """
-
-# @user_router.get("/")
-# async def get():
-#     return HTMLResponse(html_content)
--- backend/chat/services/user.py
+++ backend/chat/services/user.py
@@ -1,18 +1,27 @@
-from fastapi import HTTPException, Request, status
+from fastapi import HTTPException, Request, WebSocket, status
 from fastapi.security import OAuth2PasswordRequestForm
-from chat.utils.generate_jwt_token import create_jwt_token, verify_password
+from chat.models.message import Message
+from chat.utils.generate_jwt_token import (
+    create_jwt_token,
+    decode_jwt_token,
+    verify_password,
+)
 from chat.models.user_info import User
 from chat.schemas.user import UserCreateSchema
 from sqlalchemy.orm import Session
 
 
+active_connections = {}
+web_list = []
+user_statuses = {}
 
-def create_new_user(request:Request, user:UserCreateSchema, db:Session):
+
+def create_new_user(request: Request, user: UserCreateSchema, db: Session):
     user_exists = db.query(User).filter(User.username == user.username).first()
 
     if user_exists:
         raise HTTPException(status_code=403, detail="User Already Exists")
-    
+
     user = User(username=user.username, email=user.email, password=user.password)
     db.add(user)
     db.commit()
@@ -21,7 +30,14 @@ def create_new_user(request:Request, user:UserCreateSchema, db:Session):
 
 
 def Oauth2Login(oauth2formdata: OAuth2PasswordRequestForm, db: Session):
-    user = db.query(User).filter(User.username == oauth2formdata.username, User.password==oauth2formdata.password).first()
+    user = (
+        db.query(User)
+        .filter(
+            User.username == oauth2formdata.username,
+            User.password == oauth2formdata.password,
+        )
+        .first()
+    )
     if not user:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
@@ -41,3 +57,60 @@ def Oauth2Login(oauth2formdata: OAuth2PasswordRequestForm, db: Session):
 
     token = {"access_token": access_token, "token_type": "bearer"}
     return token
+
+
+async def handle_websocket_connection(websocket, id):
+    web_list.append(websocket)
+    jwt_token = websocket.headers.get("Authorization")
+    user_id = decode_jwt_token(jwt_token)
+    active_connections[user_id] = websocket
+    status_message = {"user_id": user_id, "status": "online"}
+    await websocket.send_json(status_message)
+    return user_id
+
+
+async def handle_received_message(data, user_id, id, db):
+    if id in active_connections:
+        new_message = Message(sender_id=user_id, receiver_id=id, content=data)
+        db.add(new_message)
+        db.commit()
+        db.refresh(new_message)
+        await broadcast_message(data, active_connections[id])
+    else:
+        new_message = Message(sender_id=user_id, receiver_id=id, content=data)
+        db.add(new_message)
+        db.commit()
+        db.refresh(new_message)
+
+
+async def handle_disconnect(user_id):
+    if user_id in active_connections:
+        del active_connections[user_id]
+
+
+async def handle_cleanup(websocket, user_id):
+    if websocket.client_state == "connected":
+        del active_connections[user_id]
+        await websocket.close()
+
+
+async def broadcast_message(message, sender_websocket: WebSocket):
+    await sender_websocket.send_text(message)
+
+
+async def handle_group_chat_connection(websocket):
+    await websocket.accept()
+    web_list.append(websocket)
+    active_connections["group"] = websocket
+
+
+async def handle_group_chat_message(websocket):
+    data = await websocket.receive_text()
+    for active_connection_key, active_connection in active_connections.items():
+        if active_connection_key != "group":
+            await broadcast_message(data, active_connection)
+
+
+async def handle_group_chat_cleanup():
+    if "group" in active_connections:
+        del active_connections["group"]
